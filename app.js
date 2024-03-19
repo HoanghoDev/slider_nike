fe2babc860f9905480924070619a83c8b8dc2810
h_ho <hohoang.dev@gmail.com>
2024-03-19 14:56:31 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+let prevBtn = document.getElementById('prev');
+let nextBtn = document.getElementById('next');
+let carousel = document.querySelector('.carousel');
+let items = carousel.querySelectorAll('.list .item');
+let indicator = carousel.querySelector('.indicators');
+let dots = indicator.querySelectorAll('.indicators ul li');
+
+let active = 0;
+let firstPosition = 0;
+let lastPosition = items.length - 1;
+
+const setSlider = () => {
+    let itemActiveOld = carousel.querySelector('.list .item.active');
+    if(itemActiveOld) itemActiveOld.classList.remove('active');
+    items[active].classList.add('active');
+
+    let dotActiveOld = indicator.querySelector('.indicators ul li.active');
+    if(dotActiveOld) dotActiveOld.classList.remove('active');
+    dots[active].classList.add('active');
+
+    indicator.querySelector('.number').innerText = '0' + (active + 1);
+}
+setSlider();
+
+nextBtn.onclick = () => {
+    active = active + 1 > lastPosition ? 0 : active + 1;
+    carousel.style.setProperty('--calculation', 1);
+    setSlider();
+}
+prev.onclick = () => {
+    active = active - 1 < firstPosition ? lastPosition : active - 1;
+    carousel.style.setProperty('--calculation', -1);
+    setSlider();
+}
